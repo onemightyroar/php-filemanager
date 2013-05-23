ec4aeec5e250d9071353f62f05501753c3bcd432
Trevor Suarez <rican7@gmail.com>
2013-05-22 23:09:11 -0400

    Boom! Tested VERY thoroughly

--- FileManager/ImageObjectTest.php
+++ FileManager/ImageObjectTest.php
@@ -113,4 +113,40 @@ class ImageObjectTest extends AbstractFileObjectTest
 
         $this->assertSame($size['bits'], $image_obj->getBits());
     }
+
+    public function testMultipleFormatCompliance()
+    {
+        $test_file_jpg = $this->getTestFileByBaseName('photo.jpg');
+        $test_file_base64 = $this->getTestFileByBaseName('photo.base64');
+
+        $wrapped_binary = ImageObject::createFromBinary(file_get_contents($test_file_jpg));
+        $wrapped_base64 = ImageObject::createFromBase64Encoded(file_get_contents($test_file_base64));
+        $raw_binary = new ImageObject($test_file_jpg);
+        $raw_base64_text = new ImageObject('data://image/jpeg;base64,'. file_get_contents($test_file_base64));
+
+        // Transitively check equality
+        $this->assertSame($wrapped_binary->getMimeType(), $wrapped_base64->getMimeType());
+        $this->assertSame($wrapped_binary->getMimeType(), $raw_binary->getMimeType());
+        $this->assertSame($wrapped_binary->getMimeType(), $raw_base64_text->getMimeType());
+
+        $this->assertSame($wrapped_binary->getMimeType(), $wrapped_binary->detectMimeType());
+        $this->assertSame($wrapped_binary->detectMimeType(), $wrapped_base64->detectMimeType());
+        $this->assertSame($wrapped_binary->detectMimeType(), $raw_binary->detectMimeType());
+        $this->assertSame($wrapped_binary->detectMimeType(), $raw_base64_text->detectMimeType());
+
+        $this->assertSame($wrapped_binary->detectMimeType(), $wrapped_binary->getMime());
+        $this->assertSame($wrapped_binary->getMime(), $wrapped_base64->getMime());
+        $this->assertSame($wrapped_binary->getMime(), $raw_binary->getMime());
+        $this->assertSame($wrapped_binary->getMime(), $raw_base64_text->getMime());
+
+
+        $this->assertSame($wrapped_binary->getRaw(), $wrapped_base64->getRaw());
+        $this->assertSame($wrapped_binary->getRaw(), $raw_binary->getRaw());
+        $this->assertSame($wrapped_binary->getRaw(), $raw_base64_text->getRaw());
+
+
+        $this->assertSame($wrapped_binary->getBase64(), $wrapped_base64->getBase64());
+        $this->assertSame($wrapped_binary->getBase64(), $raw_binary->getBase64());
+        $this->assertSame($wrapped_binary->getBase64(), $raw_base64_text->getBase64());
+    }
 }
