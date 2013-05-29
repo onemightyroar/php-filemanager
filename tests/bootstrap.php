296d10694ffa23005ea0c9afd64fe1d671f00f3b
Trevor Suarez <rican7@gmail.com>
2013-05-29 14:09:29 -0400

    Adding basic tests framework and composer file ... for PHPUnit dependency

---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+// Load our autoloader, and add our Test class namespace
+$autoloader = require( __DIR__ . '/../vendor/autoload.php' );
+$autoloader->add('OneMightyRoar\PhpFileManager\Tests', __DIR__);
